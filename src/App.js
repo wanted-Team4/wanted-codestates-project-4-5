2b000bd64eeb7354e1ecc3d8ae3896c74a1653bd
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-02-23 01:54:04 +0900

    ✨ Feat : canva를 이용한 Box그리기

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import Question1 from "./pages/Question1";
 import Question2 from "./pages/Question2";
 import styled from "styled-components";
 import SearchResult from "./pages/SearchResult";
+import store from "./store/configure";
+import {Provider} from "react-redux";
+
+
 
 const App = () => {
   return (
     <BrowserRouter>
       {/* 여기에 네비게이션 두시면 됩니다. */}
-      <MainContainer>
-        <Routes>
-          <Route exact path="/" element={<SearchResult />} />
-          {/* <Route exact path="/" element={<Index />} /> */}
-          <Route exact path="/question1" element={<Question1 />} />
-          <Route exact path="/question2" element={<Question2 />} />
-        </Routes>
-      </MainContainer>
+      <Provider store={store}>
+        <MainContainer>
+          <Routes>
+            <Route exact path="/" element={<SearchResult />} />
+            {/* <Route exact path="/" element={<Index />} /> */}
+            <Route exact path="/question1" element={<Question1 />} />
+            <Route exact path="/question2" element={<Question2 />} />
+          </Routes>
+        </MainContainer>
+      </Provider>
     </BrowserRouter>
   );
 };
--- src/reducers/initalState/index.js
+++ src/reducers/initalState/index.js
@@ -2,14 +2,14 @@ const coordinateDummy = [
   {
     x: 2,
     y: 2,
-    width: 10,
-    height: 10,
+    width: 100,
+    height: 100,
   },
   {
-    x: 2,
-    y: 2,
-    width: 10,
-    height: 10,
+    x: 50,
+    y: 50,
+    width: 100,
+    height: 100,
   },
 ];
 
