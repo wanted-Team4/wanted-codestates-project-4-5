f79775e0e301286e43adf4bc8379b64a1ed2ac6b
icallitnewart <icallitnewart@gmail.com>
2022-02-22 23:21:55 +0900

    :package: Chore : axios 추가 및 업데이트

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,24 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.nav`
+    width: 100%;
+    padding: 10px 20px;
+`;
+
+const Logo = styled.img`
+    width: 70px;
+`;
+
 const Nav = ()=> {
     return (
-        <>
-            <div>test</div>
-        </>
+        <Container>
+            <h1>
+                <Link to="/question1">
+                    <Logo src={process.env.PUBLIC_URL + "/image/logo_pxl_b.png"} alt="로고 이미지" />
+                </Link>
+            </h1>
+        </Container>
     )
 }
 
