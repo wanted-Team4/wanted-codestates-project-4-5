fd0a57965c8df620062112358445ae8998ca4f01
Kth <vcro12123@naver.com>
2022-02-23 20:51:16 +0900

    :lipstick: style :  Items 레이아웃 수정

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import Post from "../components/Post";
 import Nav from "../components/Nav";
 import Items from "../components/Items";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { useState } from "react";
 import Loading from "../components/Loading";
 
@@ -15,53 +15,35 @@ const MainContainer = styled.div`
 `;
 
 const Container = styled.div`
-    margin: 0 3em;
-    width:100%;
-    display:flex;
-    justify-content:left;
-    flex-wrap: wrap;
+  margin: 0 3em;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
 
-        @media screen and (max-width: 40rem) {
-        margin: 0 3em;
-    }
-        @media screen and (max-width: 30rem) {
-        margin: 0 1.4em;
-    }
+  @media screen and (max-width: 40rem) {
+    margin: 0 3em;
+  }
+  @media screen and (max-width: 30rem) {
+    margin: 0 1.4em;
+  }
 `;
 
 const Box = styled.div`
-    margin-left: 22em;
-    width:100%;
-    display:flex;
-    justify-content:left;
-    flex-wrap: wrap;
-
-    @media screen and (max-width: 40rem) {
-        margin-left: 2em;
-        margin-top: 45em;
-    }
-    @media screen and (max-width: 30rem) {
-        margin-left: 3.5em;
-        margin-top: 45em;
-    }
-`;
-
-const BtnContainer = styled.div`
+  margin-left: 22em;
+  width: 100%;
   display: flex;
-  justify-content: center;
-  margin: 50px 0px 100px;
-`;
+  justify-content: left;
+  flex-wrap: wrap;
 
-const MoreBtn = styled.button`
-  margin: 0 auto;
-  background: #8a39e1;
-  padding: 10px 15px;
-  border: 0;
-  border-radius: 10px;
-  color: #fff;
-  font: bold 14px/1 "inherit";
-  letter-spacing: 1px;
-  cursor: pointer;
+  @media screen and (max-width: 40rem) {
+    margin-left: 2em;
+    margin-top: 45em;
+  }
+  @media screen and (max-width: 30rem) {
+    margin-left: 3.5em;
+    margin-top: 45em;
+  }
 `;
 
 const SearchResult = () => {
@@ -69,19 +51,7 @@ const SearchResult = () => {
   const item = posts[0];
   const post = posts[1].similarResults;
 
-  //로딩
   const [isLoading, setIsLoading] = useState(true);
-  //검색어 타입이 키워드인지 코드/url인지 구분
-  const isKeyword = posts[1].hasOwnProperty("similarResults") ? false : true;
-  //검색 목록 개수
-  const totalLen = isKeyword ? posts.length : post.length;
-
-  //보여지는 포스트 개수
-  const [postIndex, setPostIndex] = useState(30);
-  //보여지는 포스트 목록 배열
-  const showPosts = isKeyword
-    ? posts.filter((item, index) => index < postIndex)
-    : post.filter((item, index) => index < postIndex);
 
   return (
     <>
@@ -89,7 +59,7 @@ const SearchResult = () => {
       <Nav />
       {posts[0].name ? (
         <MainContainer>
-          {showPosts.map((post, index) => (
+          {posts.map((post) => (
             <Post
               key={post.product_code}
               post={post}
@@ -101,7 +71,7 @@ const SearchResult = () => {
         <Container>
           <Items item={item} />
           <Box>
-            {showPosts.map((post, index) => (
+            {post.map((post) => (
               <Post
                 key={post.product_code}
                 post={post}
@@ -110,12 +80,6 @@ const SearchResult = () => {
             ))}
           </Box>
         </Container>
-      )}
-
-      {totalLen >= postIndex && (
-        <BtnContainer>
-          <MoreBtn onClick={() => setPostIndex(postIndex + 30)}>MORE</MoreBtn>
-        </BtnContainer>
       )}
     </>
   );
