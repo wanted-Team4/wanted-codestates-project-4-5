3eb85f1e98ed516c0c38a2f6c84e10e275f27ced
Kth <vcro12123@naver.com>
2022-02-23 13:40:43 +0900

    :books:  docs : Loading 페이지 추가

--- src/pages/SearchResult.js
+++ src/pages/SearchResult.js
@@ -2,66 +2,70 @@ import styled from "styled-components";
 import Post from "../components/Post";
 import Nav from "../components/Nav";
 import Items from "../components/Items";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
+import Loading from "../components/Loading";
+import { useEffect, useState } from "react";
 
 const MainContainer = styled.div`
-    margin: 0 3em;
-    width:100%;
-    display:flex;
-    justify-content:left;
-    flex-wrap: wrap;
+  margin: 0 3em;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
 `;
 
 const Container = styled.div`
-    margin: 0 3em;
-    width:100%;
-    display:flex;
-    justify-content:left;
-    flex-wrap: wrap;
+  margin: 0 3em;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
 `;
 
 const Box = styled.div`
-    margin-left: 22em;
-    width:100%;
-    display:flex;
-    justify-content:left;
-    flex-wrap: wrap;
+  margin-left: 22em;
+  width: 100%;
+  display: flex;
+  justify-content: left;
+  flex-wrap: wrap;
 `;
 
 const SearchResult = () => {
-    const posts = useSelector((state) => state.posts);
-    const item = posts[0]
-    const post = posts[1].similarResults
+  const posts = useSelector((state) => state.posts);
+  const item = posts[0];
+  const post = posts[1].similarResults;
+  const [lodingAni, setLodingAni] = useState(false);
+  useEffect(() => {
+    setLodingAni(true);
+  }, posts);
 
-    return (
+  return (
+    <>
+      {lodingAni === true ? (
         <>
-            <Nav />
-            {posts[0].name ? (
-                <MainContainer>
-                    {posts.map((post) => (
-                        <Post
-                            key={post.product_code}
-                            post={post}
-                        />
-                    ))}
-                </MainContainer>
-            ) : (
-                <Container>
-                    <Items item={item} />
-                    <Box>
-                        {post.map((post) => (
-                            <Post
-                                key={post.product_code}
-                                post={post}
-                            />
-                        ))}
-
-                    </Box>
-                </Container>
-            )}
+          <Nav />
+          {posts[0].name ? (
+            <MainContainer>
+              {posts.map((post) => (
+                <Post key={post.product_code} post={post} />
+              ))}
+            </MainContainer>
+          ) : (
+            <Container>
+              <Items item={item} />
+              <Box>
+                {post.map((post) => (
+                  <Post key={post.product_code} post={post} />
+                ))}
+              </Box>
+            </Container>
+          )}
         </>
-    );
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 };
 
 export default SearchResult;
-
