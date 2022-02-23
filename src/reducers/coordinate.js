08afda3c581d529bc9b415071544da60877acc0e
slobbie <haeru9410@gmail.com>
2022-02-23 14:37:54 +0900

    :sparkles: feat : bord 에 글쓰기 출력 , 더미 데이터에 임시데이터 추가 , 삭제기능 임시코드작성

--- src/reducers/coordinate.js
+++ src/reducers/coordinate.js
@@ -2,15 +2,16 @@ import { coordinateDummy } from "./initalState";
 import * as Constants from "../constants";
 
 const reducer = (state = coordinateDummy, action) => {
-  
   switch (action.type) {
     case Constants.ADD_COORDINATE:
       return {
-        coordinate: [...state.coordinate, action.data]
+        coordinate: [...state.coordinate, action.data],
       };
     case Constants.DELETE_COORDINATE:
       return {
-        ...state,
+        DeleteCoordinate: [
+          ...state.coordinated.filter((state) => state.id !== action.id),
+        ],
       };
     default:
       return state;
